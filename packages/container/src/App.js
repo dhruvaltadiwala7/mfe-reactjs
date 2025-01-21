886c8a3edf2bb9169f743a9b0868725ef09bd6bd
dhruvaltadiwala7 <tadiwaladhruval7ent@gmail.com>
2025-01-21 10:45:50 +0530

    Added authentication on micro-frontend

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Header from "./components/Header";
@@ -12,14 +12,19 @@ const generateClassName = createGenerateClassName({
 })
 
 export default () => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false)
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
+                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
                     <React.Suspense fallback={<Progress />}>
                         <Switch>
-                            <Route path="/auth" component={AuthLazy} />
+                            <Route path="/auth">
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
                             <Route path="/" component={MarketingLazy} />
                         </Switch>
                     </React.Suspense>
